76e98b0cae57083e882d551eb5511948be7188ef
Ally <allier0416@gmail.com>
2018-09-30 22:12:26 -0700

    First commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Jue Chen on 9/30/18.
+//  Copyright © 2018 Jue Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
